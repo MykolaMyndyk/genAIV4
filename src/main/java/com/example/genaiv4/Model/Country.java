7ac9ee38b40a7ba55acc3faf752056072b2fab2c
mmynd <mmynd@>
2023-08-09 14:32:17 +0300

    main: working version v1

---
+++ src/main/java/com/example/genaiv4/Model/Country.java
@@ -0,0 +1,10 @@
+package com.example.genaiv4.Model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Country {
+
+    @JsonProperty("startOfWeek")
+    public String startOfWeek;
+
+}
--- src/main/java/com/example/genaiv4/controller/CountriesController.java
+++ src/main/java/com/example/genaiv4/controller/CountriesController.java
@@ -1,6 +1,14 @@
 package com.example.genaiv4.controller;
 
+import com.example.genaiv4.Model.Country;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.*;
+import java.net.*;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/countries")
@@ -8,8 +16,50 @@ public class CountriesController {
 
     @GetMapping("/get")
     @ResponseBody
-    public String getFoos(@RequestParam String id) {
-        return "ID: " + id;
+    public List<Country> getFoos(@RequestParam(required = false) String country, @RequestParam(required = false) String population,
+                          @RequestParam(required = false) String sort, @RequestParam(required = false) String pagination) {
+
+        List<Country> result = getResponse();
+
+
+        return result;
+    }
+
+    private List<Country> getResponse() {
+        try {
+            URL url = new URL("https://restcountries.com/v3.1/all");
+//            String query = INSERT_HERE_YOUR_URL_PARAMETERS;
+
+            //make connection
+            URLConnection urlc = url.openConnection();
+
+            //use post mode
+            urlc.setDoOutput(true);
+            urlc.setAllowUserInteraction(false);
+
+            InputStream inputStream = urlc.getInputStream();
+
+
+            //get result
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            br.close();
+
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+            List<Country> myObjects = mapper.readValue(sb.toString(), new TypeReference<List<Country>>(){});
+//            List<Country> countries = mapper.readValue(sb.toString(), List<>Country.class);
+
+            return myObjects;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
